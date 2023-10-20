a8b6c10b0a15642a565f6f89866ffeab5ce26add
dmitrykrtv <dmitry.korotayev@gmail.com>
2023-10-21 02:14:14 +0300

    Добавлен Open CV, MSS и реализовано получение скриншотов и нахождения количества сохраняемых скриншотов

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # csbot_beginning
+
+Пробная версия cs go бота по образцу хаудиХо
+
+### Переключить evn
+``` bash
+.\myenv\Scripts\activate
+```
+
+### Сохранить список пакетов evn
+``` bash
+pip freeze > .\requirements.txt
+```
--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-import autoit
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 
-autoit.run("notepad.exe")
-autoit.win_wait_active("[CLASS:Notepad]", 3)
+import time 
 
-# autoit.control_send("[CLASS:Notepad]", "Edit1", "hello world{!}")
+import cv2
+import mss
+import numpy
+        
 
+def screen_record_efficiency():
+    mon = {"top": 0, "left": 0, "width": 1280, "height": 720}
+    
+    title = "[MSS] FPS benchmark" 
+    new_frame_time, prev_frame_time, fps = 0, 0, 0
+    sct = mss.mss()                         # инициализировать mss
+    last_time = time.time()    
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    last_time = time.time()                 
+    
+    while True:
+        img = numpy.asarray(sct.grab(mon))  # получаем картинку
+        
+        new_frame_time = time.time()
+        fps = 1/(new_frame_time - prev_frame_time)
+        prev_frame_time = new_frame_time
+        fps = int(fps)
+    
+        cv2.putText(img, str(fps), (7, 40), font, 1, (100, 255, 0), 3, cv2.LINE_AA)  # показывем фпс
+        
+        imS = cv2.resize(img, (854, 480))        
+        cv2.imshow(title, imS)
+        if cv2.waitKey(25) & 0xFF == ord("q"):  # ord("q") - вернуть позициб символа в юникоде
+            cv2.destroyAllWindows()
+            break
+    
+    return fps
+
+
+screen_record_efficiency()
